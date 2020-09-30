e49d751872a618e2e0e32ae48e351b6c26397f6b
jfmaes <55604242+jfmaes@users.noreply.github.com>
2020-09-30 17:31:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace SharpXOR
+{
+    class Program
+    {
+        public static byte[] XOR(byte [] payload, string XORKey)
+        {
+           byte[] xorStuff = new byte[payload.Length] ;
+           char[] bXORKey = XORKey.ToCharArray();
+           for(int i = 0; i< payload.Length;i++)
+            {
+                xorStuff[i] = (byte)(payload[i] ^ bXORKey[i % bXORKey.Length]);
+            }
+            return xorStuff;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("absolute path of file to xor");
+            String inputPath = Console.ReadLine();
+            Console.WriteLine("XOR key ");
+            String key = Console.ReadLine();
+            Console.WriteLine("absolute path to write output to ");
+            String outputPath = Console.ReadLine();
+            byte[] payload = File.ReadAllBytes(inputPath);
+            byte[] stuff = XOR(payload, key);
+            File.WriteAllBytes(outputPath,stuff);
+            Console.WriteLine("successfully XOR'd %s! \n Data written to %s",inputPath,outputPath);
+
+        }
+    }
+}
